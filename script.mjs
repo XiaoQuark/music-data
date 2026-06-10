e507831831c636977651e01b8d8d7f8c9f5c2cf4
XiaoQuark <kris.oldrini@gmail.com>
2026-06-10 11:44:16 +0100

    feature: handle user change

--- script.mjs
+++ script.mjs
@@ -1,27 +1,41 @@
-import { getUserIDs } from "./data.mjs";
+import { getUserIDs, getListenEvents } from "./data.mjs";
 
 const state = {
 	users: null,
+	selectedUser: null,
+	userListenEvents: [],
 };
 
 const elements = {
 	userSelect: null,
+	userHeading: "",
 };
 
 window.onload = function () {
 	elements.userSelect = document.getElementById("user-select");
+	elements.userHeading = document.getElementById("user-heading");
 	// document.querySelector("body").innerText =
 	// 	`There are ${countUsers()} users`;
 	state.users = getUserIDs();
 
 	populateUserDropdown();
+
+	elements.userSelect.addEventListener("change", handleUserChange);
 };
 
 function populateUserDropdown() {
 	for (const user of state.users) {
 		const option = document.createElement("option");
-		option.value = "user";
+		option.value = user;
 		option.textContent = `User ${user}`;
 		elements.userSelect.appendChild(option);
 	}
+}
+
+function handleUserChange() {
+	state.selectedUser = event.target.value;
+	console.log(state.selectedUser);
+	state.userListenEvents = getListenEvents(state.selectedUser) || [];
+	console.log(state.userListenEvents);
+	elements.userHeading.textContent = `User ${state.selectedUser} Listening Stats`;
 }
